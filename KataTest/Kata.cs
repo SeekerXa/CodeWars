3ce8627faf6815a7a475c7e7b88725cf64171be2
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-24 17:36:32 +0200

    Task1
    In descending order: [10, 2, 1]
    Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
    
    /If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell ).

---
+++ KataTest/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTest
+{
+    internal class Kata
+    {
+        //        In descending order: [10, 2, 1]
+
+        //        Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+        //If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell ).
+        public static int SumOfDifferences(int[] arr)
+        {
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            int sum = 0;
+            if (arr == null  || arr.Length < 2) return sum;
+            else
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    sum += arr[i - 1] - arr[i];
+                }
+            return sum;
+        }
+    }
+}
